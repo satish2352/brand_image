3b4a8aa02525fd0c44390e4dc86de2ed32c8ef45
Vivek Patil <vivekpatilss23@gmail.com>
2025-12-29 17:20:03 +0530

    detail page and campain working

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -35,6 +35,8 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"> --}}
+
 
 </head>
 
