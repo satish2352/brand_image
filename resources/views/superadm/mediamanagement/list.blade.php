449de554cd268539522a3e0191cd3899a8dc10ec
Vivek Patil <vivekpatilss23@gmail.com>
2026-01-16 12:36:47 +0530

    resp tbl

--- resources/views/superadm/mediamanagement/list.blade.php
+++ resources/views/superadm/mediamanagement/list.blade.php
@@ -111,6 +111,7 @@
                                 </div>
                             </div>
                         </form>
+                    <div class="table-responsive">
                         <table class="table table-bordered table-striped">
 
                             {{-- <table class="table table-bordered table-striped datatables"> --}}
