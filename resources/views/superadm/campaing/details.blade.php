653815720fc7ac48707ee65a77bceba3e5b36eb2
Savita Hajare <hajaresavita04@gmail.com>
2026-01-22 15:21:16 +0530

    changes in code

--- resources/views/superadm/campaing/details.blade.php
+++ resources/views/superadm/campaing/details.blade.php
@@ -55,7 +55,7 @@
                                             </a>
 
                                             <a href="{{ route('campaign.export.ppt', base64_encode($campaignId)) }}"
-                                                class="btn btn-warning btn-sm">
+                                                class="btn btn-success btn-sm">
                                                 Export PPT
                                             </a>
                                         </td>
@@ -82,7 +82,7 @@
                                                 <input type="hidden" name="price" value="{{ $item->price }}">
                                                 <input type="hidden" name="total_price" value="{{ $item->total_price }}">
                                                 <input type="hidden" name="total_days" value="{{ $item->total_days }}">
-                                                {{-- <input type="hidden" name="campaign_id" value="{{ $item->campaign_id }}"> --}}
+                                                <input type="hidden" name="campaign_id" value="{{ $item->campaign_id }}">
 
 
 
