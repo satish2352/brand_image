2642e5a73e90c388d4185f4be25c4be50aef1af6
Savita Hajare <hajaresavita04@gmail.com>
2026-02-06 11:52:34 +0530

    webhook testing

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,7 +19,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-         'payment/webhook/razorpay',
+          'payment/webhook/razorpay',
+    'payment/webhook/razorpay/*',
        
     ];
 }
