8ea55afec17a39dc453a7e312e340fc56673fd5f
Savita Hajare <hajaresavita04@gmail.com>
2026-02-06 11:23:41 +0530

    changes in ppt

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Website\CheckoutController;
 
-Route::post('/api/payment/webhook/razorpay', [CheckoutController::class, 'razorpayWebhook']);
+Route::post('/payment/webhook/razorpay', [CheckoutController::class, 'razorpayWebhook']);
 
 
 
