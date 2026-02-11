61216f06253e96dbb1e906e56b11e8b8ad44ab44
Savita Hajare <hajaresavita04@gmail.com>
2026-02-11 16:01:53 +0530

    changes in  cart

--- resources/views/website/cart.blade.php
+++ resources/views/website/cart.blade.php
@@ -1,7 +1,5 @@
 @extends('website.layout')
-
 @section('title', 'My Cart')
-
 @section('content')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
