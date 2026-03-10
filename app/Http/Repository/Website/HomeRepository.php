5a99944d97b68e5684c3a340efdd7c2cd339df9b
Savita Hajare <hajaresavita04@gmail.com>
2026-03-10 09:38:46 +0530

    changes in size

--- app/Http/Repository/Website/HomeRepository.php
+++ app/Http/Repository/Website/HomeRepository.php
@@ -264,13 +264,9 @@ END AS is_available_days
             'max_area' => $filters['max_area'] ?? null
         ]);
         //  PAGINATION (REQUIRED FOR LAZY LOADING)
-        $results = DB::query()
-            ->fromSub($query, 'media')
-            ->orderBy('id', 'DESC')
+        $results = $query
+            ->orderBy('m.id', 'DESC')
             ->paginate(config('fileConstants.PAGINATION'));
-        // $results = $query
-        //     ->orderBy('m.id', 'DESC')
-        //     ->paginate(config('fileConstants.PAGINATION'));
 
         return $results;
     }
